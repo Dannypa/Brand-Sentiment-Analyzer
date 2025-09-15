bd3263c639920ccd6c58f5dda6acc97b67912187
gnud-gnaoh <hoangdung2.bubu@gmail.com>
2025-09-15 19:54:55 +0700

    add some sentiment analysis

--- reddit/dung/main.py
+++ reddit/dung/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import praw
 import os
+from transformers import pipeline
 
 load_dotenv()
 
@@ -11,17 +12,39 @@ reddit = praw.Reddit(
     user_agent=f"testscript by u/{os.getenv('REDDIT_USERNAME')}",
     username=os.getenv("REDDIT_USERNAME"),
 )
-
 print(reddit.user.me())
 
 all = reddit.subreddit("all") # all subreddits
 
-submissions = [submission for submission in all.search("nike", time_filter="month")]
+# list_of_topics = ["climate", "sustainability", "diversity", "inclusion", "equity"] 
+def sentiment_on_topic(brand, topic):
+    pass
 
-print(f"Found {len(submissions)} submissions")
+def attention(brand):
+    pass
 
-for submission in submissions:
-    print(submission.title)
-    print(submission.created_utc)
-    print(submission.score)
-    print()
+sentiment_pipeline = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+
+def sentiment(brand) -> float:
+    submissions = [submission for submission in all.search(brand, time_filter="month")]
+    # print(f"Found {len(submissions)} submissions")
+    sentiments = [sentiment_pipeline(submission.title) for submission in submissions]
+    
+    # # print 5 sample sentiments
+    # for i in range(min(5, len(sentiments))):
+    #     print(f"Title: {submissions[i].title}")
+    #     print(f"Sentiment: {sentiments[i]}")
+
+    score = 0.0
+    for sentiment in sentiments:
+        if sentiment[0]['label'] == 'POS':
+            score += sentiment[0]['score']
+        elif sentiment[0]['label'] == 'NEG':
+            score -= sentiment[0]['score']
+    
+    return (score / len(submissions)) if len(submissions) > 0 else 0.0
+
+list_of_brands = ["nike", "adidas", "puma", "reebok", "new balance"]
+
+for brand in list_of_brands:
+    print(f"Sentiment for {brand}: {sentiment(brand)}")
