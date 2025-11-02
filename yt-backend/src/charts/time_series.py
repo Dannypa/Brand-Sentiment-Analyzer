fad359d3d65300c3be48b7517ee6972a0206b8c7
Daniil Parniukov <dparniukov@gmail.com>
2025-11-02 22:38:21 +0200

    made the timeseries vis to work; moved everything to src :cry; created a separate ml file.

--- yt-backend/src/charts/time_series.py
+++ yt-backend/src/charts/time_series.py
@@ -5,38 +5,53 @@ from dateutil.relativedelta import relativedelta
 from ytapi import search_videos, get_comments
 from dataclasses import dataclass
 import pandas as pd
-
-# todo: move to separate common file!!!!
-def get_sentiment(texts: list[str]):
-    import requests
-    import json
-    address = "http://ml:8080/get_sentiment"
-    data = json.dumps([{"texts": texts}])
-    sentiment = requests.post(address, data=data).json()[0]
-    return sentiment
+from ml import get_sentiment
 
 
-def get_timerange():
-    oldest_year = dt.datetime.now().year - 2
-    current_date = dt.datetime(year=oldest_year, month=1, day=1)
+QUERY_SPAN = relativedelta(months=12)
+OLDEST_DATE = dt.datetime.now() - relativedelta(years=1)
+QUERY_SUFFIXES = ("",)
+# QUERY_SUFFIXES = ("", " good", " bad")
+
+VISUALISATION_SECTION = relativedelta(months=1)
+
+
+def get_timerange(step: relativedelta):
+    current_date = OLDEST_DATE
     while current_date < dt.datetime.now():
         yield current_date
-        current_date += relativedelta(month=1)
+        current_date += step
+
+
+def get_download_timerange():
+    yield from get_timerange(QUERY_SPAN)
+    
+def get_visualisation_timerange():
+    yield from get_timerange(VISUALISATION_SECTION) 
 
 @dataclass
 class Video:
     title: str
     vid: str
+    published_at: dt.datetime
 
 def get_videos(brand: str, start_date: dt.datetime, end_date: dt.datetime):
+    # print("in get_videos")
+    # print(QUERY_SUFFIXES)
+    # for suff in QUERY_SUFFIXES: 
+        # print(f"There is a suff: {suff}")
     result = []
-    for suffix in ["", " good", " bad"]:
+    for suffix in QUERY_SUFFIXES:
+        # print("inside for loop....")
         query = brand + suffix
+        # print(f"query: {query}")
+        # print(start_date, end_date)
         videos = search_videos(query, start_date=start_date, end_date=end_date)
         for video in videos.items:
             title = video.snippet.title
-            vid = video.id.videoId 
-            result.append(Video(title, vid))
+            vid = video.id.videoId
+            published_at = video.snippet.publishedAt
+            result.append(Video(title, vid, published_at))
     return result
 
 
@@ -48,28 +63,47 @@ def get_comment_texts(vid: str) -> list[str]:
     return comments
 
 
-
 def video_sentiment(video: Video) -> float:
+    # print("here lol")
+    # print(get_sentiment([video.title]))
+    # print("after here lol")
     title_sentiment = float(get_sentiment([video.title])[0])
+    # print("Sentiment of the title:", title_sentiment)
 
     comment_sentiment = get_sentiment(get_comment_texts(video.vid))
+    # print("Comment sentiment: ", comment_sentiment)
 
     return (title_sentiment + sum(comment_sentiment)) / (len(comment_sentiment) + 1) # weight of 1 comment to title for now; change later
    
+def get_all_videos(brands: list[str]) -> dict[str, list[Video]]:
+        result = {brand: [] for brand in brands}
+        for brand in brands:
+            for start_date in get_download_timerange():
+                end_date = start_date + QUERY_SPAN
+                result.setdefault(brand, []).extend(get_videos(brand, start_date, end_date))
+                
+        return result
+
 
 def time_series_sentiment(brands: list[str]) -> str:
     dates = [] # todo: query vids once??
     sentiment = {brand: [] for brand in brands}
 
-    for start_date in get_timerange():
-        end_date = start_date + relativedelta(month=1)
+    videos = get_all_videos(brands)
+    print(len(videos))
+
+    for start_date in get_visualisation_timerange():
         dates.append(start_date)
+        end_date = start_date + VISUALISATION_SECTION
 
         for brand in brands:
-            
-            videos = get_videos(brand, start_date, end_date)
+
+            # print(brand, start_date)
+            section = [video for video in videos[brand] if start_date.timestamp() <= video.published_at.timestamp() <= end_date.timestamp()]
+            # print(videos)
             temp_sentiment = []
-            for video in videos:
+            for video in section:
+                print(video)
                 try:
                     temp_sentiment.append(video_sentiment(video))
                 except Exception as e:
