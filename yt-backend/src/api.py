60d40ea868c309a324dce4110fe7f251cc98356f
magimoose <mgreig814@gmail.com>
2025-11-08 12:28:14 +0100

    caching works better and works for > 50 search queries (also rebased fyi)

--- yt-backend/src/api.py
+++ yt-backend/src/api.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI, Depends
+import io
+import json
+import os
+from typing import Optional
+
+import pandas as pd
+import plotly.express as px
+import requests
+from dotenv import load_dotenv
+from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, JsonValue
@@ -15,10 +25,20 @@ from services import video_to_dataframe, remove_videos_without_brand_title, remo
 import pandas as pd
 from charts.latest_histogram import histogram_sentiment, histogram_combined
 from charts.time_series import time_series_sentiment
+
+from charts.latest_histogram import histogram_combined, histogram_sentiment
+from charts.time_series import time_series_sentiment
 from charts.word_cloud import word_cloud
 from charts.hist import hist
 import psycopg2
 from psycopg2 import pool
+from models import Chart
+from services import (
+    remove_videos_without_brand_title,
+    remove_videos_without_comments,
+    video_to_dataframe,
+)
+from ytapi import get_comments, get_video_details, search_videos
 
 load_dotenv()
 
@@ -57,20 +77,23 @@ def get_db_connection():
         db_pool.putconn(conn)
 
 
-@api.get("/charts")
-def get_charts(brand: str, conn=Depends(get_db_connection)) -> list[Chart]:
-
+def get_charts_inner(brands: list[str], conn: psycopg2) -> list[Chart]:
     charts = []
 
     try:
         charts.extend(
             [
-                Chart(title="Sentiment histogram", plotly_json=hist([brand], conn)),
-                # Chart(title="Sentiment histogram", plotly_json=histogram_sentiment([brand], conn)),
-                #Chart(title="Combined histogram", plotly_json=histogram_combined([brand])),
-                # Chart(title="Sentiment time series", plotly_json=time_series_sentiment([brand])),
-                #Chart(title="Views time series", plotly_json=time_series_views([brand])),
-                #Chart(title="Combined time series", plotly_json=time_series_combined([brand])),
+                Chart(
+                    title="Sentiment histogram",
+                    plotly_json=hist(brands, conn),
+                ),
+                # Chart(title="Combined histogram", plotly_json=histogram_combined([brand])),
+                # Chart(
+                #     title="Sentiment time series",
+                #     plotly_json=time_series_sentiment(brands),
+                # ),
+                # Chart(title="Views time series", plotly_json=time_series_views([brand])),
+                # Chart(title="Combined time series", plotly_json=time_series_combined([brand])),
             ]
         )
     except Exception as e:
@@ -78,12 +101,16 @@ def get_charts(brand: str, conn=Depends(get_db_connection)) -> list[Chart]:
 
     return charts
 
-@api.post("/charts/multibrand")
-def get_charts_multibrand(brands: list[str]) -> list[Chart]:
-    
-    charts = []
 
-    return charts
+@api.get("/charts")
+def get_charts(brand: str, conn=Depends(get_db_connection)) -> list[Chart]:
+    return get_charts_inner([brand], conn)
+
+
+@api.post("/charts/multibrand")
+def get_charts_multibrand(brands: list[str], conn=Depends(get_db_connection)) -> list[Chart]:
+    return get_charts_inner(brands, conn)
+
 
 @api.get("/charts/wordcloud")
 def get_word_cloud(brand: str):
